95dac32f533c1e32644ff187743177e5402ed8a0
User1337 <user1337@user1337s-mac-pro.fritz.box>
2026-04-25 07:21:09 +0200

    Initial Commit

---
+++ iOS-Microphone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Microphone
+//
+//  Created by User1337 on 25.04.26.
+//  Copyright © 2026 User1337. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
